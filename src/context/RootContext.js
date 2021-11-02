2d8e4fc72ee61730cbcbf45888e093570287f85a
Sk Kaushik <skkaushik021@gmail.com>
2021-11-02 22:09:34 +0530

    data bug fixed

--- src/context/RootContext.js
+++ src/context/RootContext.js
@@ -28,7 +28,6 @@ export const RootContextProvider = (props) => {
         }
       })
       .then((data) => {
-        console.log(data);
         setRepoList(data);
         setLoading(false);
       })
